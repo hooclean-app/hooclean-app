1f024ecabce9f4c5437dc0fff911a948939fe19e
jorge <jorge.coronilla.naranjo@gmail.com>
2023-12-29 16:55:27 +0100

    [Add] JWT sigin and verification

--- app/api/auth/login/route.ts
+++ app/api/auth/login/route.ts
@@ -1,6 +1,9 @@
+import { SignJWT } from 'jose';
 import { NextRequest, NextResponse } from 'next/server';
-import jwt from 'jsonwebtoken';
+// import jwt from 'jsonwebtoken';
 import { cookies } from 'next/headers';
+import { serialize } from 'cookie';
+import { getJwtSecretKey } from '@/app/lib/auth';
 
 export async function POST(request: NextRequest) {
   try {
@@ -14,18 +17,26 @@ export async function POST(request: NextRequest) {
       );
     }
     if (data.password === 'admin' && data.email === 'admin@mail.com') {
-      const token = jwt.sign(
-        {
-          exp: Math.floor(Date.now() / 1000) + 60 * 60 * 24,
-          email: data.email,
-          password: data.password,
-        },
-        'secret'
-      );
-      cookies().set('access-token', 'we did it');
+      const token = await new SignJWT({
+        email: data.email,
+        password: data.password,
+      })
+        .setProtectedHeader({ alg: 'HS256' })
+        .setIssuedAt()
+        .setExpirationTime('1day')
+        .sign(getJwtSecretKey());
+
+      const serialiazed = serialize('access-token', token, {
+        httpOnly: true,
+        path: '/',
+        maxAge: 30 * 60 * 60 * 24,
+        sameSite: 'strict',
+        secure: process.env.NODE_ENV === 'production',
+      });
+      cookies().set('access-token', serialiazed);
 
       return NextResponse.json(
-        { message: 'Successfully logged in', token },
+        { message: 'Successfully logged in' },
         { status: 200 }
       );
     }
--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,11 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
+import { verifyJwtToken } from './app/lib/auth';
+
 const PUBLIC_FILE = /\.(.*)$/;
 
-export default function nextjs_future(request: NextRequest) {
+export default async function nextjs_future(request: NextRequest) {
   const isPublicFiles = PUBLIC_FILE.test(request.nextUrl.pathname);
 
   // Solves the problem of next middleware not loading cached css or public files
@@ -11,16 +13,22 @@ export default function nextjs_future(request: NextRequest) {
     return NextResponse.next();
   }
 
-  const cookie = request.cookies.get('sessionCookie');
-  console.log(cookie);
-  if (cookie && cookie.value === 'authcookie') {
-    const response = NextResponse.next();
+  const cookie = request.cookies.get('access-token');
+  if (cookie) {
+    const cookiePairs = cookie.value.split('; ');
+    const tokenPair = cookiePairs.find((pair) =>
+      pair.startsWith('access-token=')
+    );
 
-    return response;
+    if (tokenPair) {
+      const token = tokenPair.split('=')[1];
+      const verified = await verifyJwtToken(token);
+    } else {
+      console.log('Token not found in the cookie.');
+    }
+
+    return NextResponse.next();
   }
-  const headers = new Headers(request.headers);
-
-  console.log(headers);
 
   return NextResponse.redirect(new URL('/', request.url));
 }
